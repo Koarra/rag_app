4601b3f2b19c5b791c43c3a1c75db3474e449140
Koarra <152796199+Koarra@users.noreply.github.com>
2026-01-09 15:26:14 +0100

    Add node label updates based on entity types

--- todrop.py
+++ todrop.py
@@ -1,1 +1,16 @@
 
+# Update node labels
+for node in graph['nodes']:
+    entity_name = node['data']['name']
+    
+    # Determine entity type
+    if entity_name in persons:
+        entity_type = 'PERSON'
+    elif entity_name in companies:
+        entity_type = 'COMPANY'
+    else:
+        continue  # Skip unknown entities
+    
+    # Build label with optional suffix
+    suffix = '_FLAGGED' if entity_name in flagged_entities else ''
+    node['data']['label'] = f"{entity_type}{suffix}"
