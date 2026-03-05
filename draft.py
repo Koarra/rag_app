0927972156fd3c842c4a6f5c0af50d7c5059e9c7
Koarra <152796199+Koarra@users.noreply.github.com>
2026-03-05 14:11:07 +0100

    Update draft.py

--- draft.py
+++ draft.py
@@ -1,9 +1,2 @@
-    excluded_keys = {"client_type"}
-    kyc_dict = {
-        k: v
-        for k, v in partner_info.kyc_dataset.items()
-        if k not in excluded_keys
-        and v is not None
-        and v != ""
-        and v != []
-    }
+reason = str(value.get("reason", ""))
+self.write_bold_instances(row_cells[2], reason.strip())
